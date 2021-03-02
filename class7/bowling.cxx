5066648a50f2024b3b0d1e53e111cd28214494b6
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-02 08:34:14 -0500

    class7: add first actual (failing) test
    In fact, this doesn't even compile yet.

--- class7/bowling.cxx
+++ class7/bowling.cxx
@@ -1,3 +1,15 @@
 
 #include <gtest/gtest.h>
 
+#include <vector>
+
+// ======================================================================
+// Tests
+
+TEST(Bowling, AllZeros)
+{
+  std::vector<int> rolls = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  };
+  EXPECT_EQ(bowlingScore(rolls), 0);
+}
