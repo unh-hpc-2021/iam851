5ed021822da615bb7e3edec866d652a069fc2a36
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-03 21:09:00 -0500

    class7s: add a test with a spare and make it work
    Since we're already frame-based, it's not too difficult to handle spares.

--- class7/bowling.cxx
+++ class7/bowling.cxx
@@ -15,7 +15,12 @@ int bowlingScore(const std::vector<int>& rolls)
   int score = 0;
   for (int frame = 0; frame < 10; frame++) {
     int frame_start = 2 * frame;
-    score += rolls[frame_start] + rolls[frame_start + 1];
+    int frame_score = rolls[frame_start] + rolls[frame_start + 1];
+    if (frame_score == 10) { // spare
+      score += frame_score + rolls[frame_start + 2];
+    } else {
+      score += frame_score;
+    }
   }
   return score;
 }
@@ -37,4 +42,12 @@ TEST(Bowling, RegularGame)
     0, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6,
   };
   EXPECT_EQ(bowlingScore(rolls), 14);
+}
+
+TEST(Bowling, Spare)
+{
+  std::vector<int> rolls = {
+    0, 0, 2, 3, 4, 6, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6,
+  };
+  EXPECT_EQ(bowlingScore(rolls), 5 + 4 + 6 + 3 + 3 + 4 + 3 + 6);
 }
