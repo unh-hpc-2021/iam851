83e37a2103b23ed08b963e65a6be52eb4c729c85
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-08 21:11:14 -0500

    class8s: pass the vector length to the C linalg functions
    That's not pretty, and particularly bad for the mat-vec multiply,
    where n has to be passed first.

--- linear_algebra/c/test_matrix_vector_mul.c
+++ linear_algebra/c/test_matrix_vector_mul.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
 		    {0., 0., 3.}};
   double y[N];
 
-  matrix_vector_mul(A, x, y);
+  matrix_vector_mul(N, A, x, y);
   assert(y[0] == 3. && y[1] == 4. && y[2] == 9.);
 
   return 0;
--- linear_algebra/c/vector_dot.c
+++ linear_algebra/c/vector_dot.c
@@ -1,10 +1,10 @@
 
 #include "linear_algebra.h"
 
-double vector_dot(const double* x, const double* y)
+double vector_dot(const double* x, const double* y, int n)
 {
   double sum = 0.f;
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < n; i++) {
     sum += x[i] * y[i];
   }
   return sum;
