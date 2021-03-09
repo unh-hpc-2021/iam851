161f7620078e21aa289709b7b4ee0ed2636c493a
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-08 20:45:58 -0500

    class8s: separate out test_vector_dot.c
    This moves the "testing" code into a separate source file, adds a header
    file and adopts the build accordingly.

--- linear_algebra/c/vector_dot.c
+++ linear_algebra/c/vector_dot.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
 
-// vector length (fixed)
-
-#define N (3)
-
-// ----------------------------------------------------------------------
-// vector_dot
-//
-// returns the dot product of the two vectors
-// x: first vector
-// y: second vector
+#include "linear_algebra.h"
 
 double vector_dot(double* x, double* y)
 {
@@ -18,19 +8,4 @@ double vector_dot(double* x, double* y)
     sum += x[i] * y[i];
   }
   return sum;
-}
-
-// ----------------------------------------------------------------------
-// main
-//
-// test the vector_dot() function
-
-int main(int argc, char** argv)
-{
-  double x[N] = {1., 2., 3.};
-  double y[N] = {2., 3., 4.};
-
-  printf("dot product is %g\n", vector_dot(x, y));
-
-  return 0;
 }
