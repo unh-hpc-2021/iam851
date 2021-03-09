3369960a3c3416faebe51cd61c7b45025875f796
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-08 20:52:39 -0500

    class8s: add matrix_vector_mul and test
    fixed-size multi-dimensional arrays in C aren't too bad...

---
+++ linear_algebra/c/test_matrix_vector_mul.c
@@ -0,0 +1,24 @@
+
+#include "linear_algebra.h"
+
+#include <stdio.h>
+#include <assert.h>
+
+// ----------------------------------------------------------------------
+// main
+//
+// test the matrix_vector_mul() function
+
+int main(int argc, char** argv)
+{
+  double x[N] = {1., 2., 3.};
+  double A[N][N] = {{1., 1., 0.},
+		    {0., 2., 0.},
+		    {0., 0., 3.}};
+  double y[N];
+
+  matrix_vector_mul(A, x, y);
+  assert(y[0] == 3. && y[1] == 4. && y[2] == 9.);
+
+  return 0;
+}
