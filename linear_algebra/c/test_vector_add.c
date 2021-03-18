e52dc0d87e5a7c90ac663c543fafa57732571979
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-17 21:52:57 -0400

    hw5s: convert vector_add and its test to using struct vector

--- linear_algebra/c/test_vector_add.c
+++ linear_algebra/c/test_vector_add.c
@@ -11,12 +11,26 @@
 
 int main(int argc, char** argv)
 {
-  double x[4] = {1., 2., 3., 4.};
-  double y[4] = {2., 3., 4., 5.};
-  double z[4];
+  const int N = 4;
+  struct vector x;
+  vector_construct(&x, N);
+  struct vector y;
+  vector_construct(&y, N);
+  struct vector z;
+  vector_construct(&z, N);
 
-  vector_add(x, y, z, 4);
-  assert(z[0] == 3. && z[1] == 5. && z[2] == 7. && z[3] == 9.);
+  for (int i = 0; i < N; i++) {
+    VEC(&x, i) = 1. + i;
+    VEC(&y, i) = 2. + i;
+  }
+
+  vector_add(&x, &y, &z);
+  assert(VEC(&z, 0) == 3. && VEC(&z, 1) == 5. && VEC(&z, 2) == 7. &&
+         VEC(&z, 3) == 9.);
+
+  vector_destruct(&x);
+  vector_destruct(&y);
+  vector_destruct(&z);
 
   return 0;
 }
