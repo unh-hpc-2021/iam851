3e43d81fa5faca6813fb306e6d146c82b3397c64
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-20 12:48:12 -0400

    class20s: initialize and finalize MPI

--- class20/test_mpi.cxx
+++ class20/test_mpi.cxx
@@ -4,10 +4,14 @@
 
 int main(int argc, char** argv)
 {
+  MPI_Init(&argc, &argv);
+
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   std::cout << "Hi there " << rank << "/" << size << "\n";
+
+  MPI_Finalize();
   return 0;
 }
