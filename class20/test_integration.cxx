1709d5ca211d32a1b10f3400d8ee9a86bb171093
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-22 10:49:11 -0400

    class21s: distribute integration work onto two procs
    Don't get a final answer yet, though...

--- class20/test_integration.cxx
+++ class20/test_integration.cxx
@@ -1,6 +1,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <cassert>
 
 #include <mpi.h>
 
@@ -11,17 +12,33 @@ double f(double x)
 
 int main(int argc, char** argv)
 {
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   const int N = 100000000;
 
+  int ib, ie;
+  assert(size == 2);
+  if (rank == 0) {
+    ib = 0;
+    ie = N / 2;
+  } else {
+    ib = N / 2;
+    ie = N;
+  }
   double sum = 0.;
   double dx = 1. / N;
   double t1 = MPI_Wtime();
-  for (int i = 0; i < N; i++) {
+  for (int i = ib; i < ie; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
   double t2 = MPI_Wtime();
   std::cout << "took " << t2 - t1 << " secs.\n";
   std::cout << "integral is approximately " << sum << "\n";
 
+  MPI_Finalize();
   return 0;
 }
