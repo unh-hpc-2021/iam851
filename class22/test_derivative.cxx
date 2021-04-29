69d17b7acc758f61bdfd4ee7281da83fe53fce79
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-28 23:46:56 -0400

    class23: move derivative() function into its own source file

--- class22/test_derivative.cxx
+++ class22/test_derivative.cxx
@@ -1,37 +1,13 @@
 
+#include "wave_equation.h"
+
 #include <xtensor/xcsv.hpp>
-#include <xtensor/xio.hpp>
-#include <xtensor/xpad.hpp>
-#include <xtensor/xtensor.hpp>
 
 #include <mpi.h>
 
 #include <fstream>
 #include <iostream>
 #include <cassert>
-
-xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
-{
-  const int G = 1;
-
-  int n = f.shape(0);
-  auto f_g = xt::pad(f, G);
-  f_g(G + -1) = f_g(G + n - 1);
-  f_g(G + n) = f_g(G + 0);
-
-#if 1
-  auto fprime = xt::zeros_like(f);
-  for (int i = 0; i < n; i++) {
-    fprime(i) = (f_g(i + G + 1) - f_g(i + G - 1)) / (2. * dx);
-  }
-
-  return fprime;
-#else
-  // Python equivalent: return (f_g[2:] - f_g[:-2]) / (2. * dx);
-  return (xt::view(f_g, xt::range(2, _)) - xt::view(f_g, xt::range(_, -2))) /
-         (2. * dx);
-#endif
-}
 
 int main(int argc, char** argv)
 {
