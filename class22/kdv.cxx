0d699b911ec6cb5c8c2e5c18ced91c1ecfbbd465
Kai <kaig@ln-0001.cm.cluster>
2021-05-10 22:59:30 -0400

    kdv: add timing, single file output

--- class22/kdv.cxx
+++ class22/kdv.cxx
@@ -7,34 +7,71 @@
 #include <fstream>
 #include <iostream>
 
+double t_fill_ghosts;
+double t_compute_rhs;
+double t_output;
+
 xt::xtensor<double, 1> rhs(const mpi_domain& domain,
                            const xt::xtensor<double, 1>& u)
 {
   const int G = 2;
 
-  auto u_g = xt::pad(u, G);
-  domain.fill_ghosts(u_g);
+  auto rhs = xt::zeros_like(u);
 
+  auto u_g = xt::pad(u, G);
+  t_fill_ghosts -= MPI_Wtime();
+  domain.fill_ghosts(u_g);
+  t_fill_ghosts += MPI_Wtime();
+
+  t_compute_rhs -= MPI_Wtime();
   double dx3i = 1. / (2 * std::pow(domain.dx(), 3));
   double dxi = 1. / (2. * domain.dx());
 
-  auto rhs = xt::zeros_like(u);
   for (int i = 0; i < rhs.shape(0); i++) {
     rhs(i) = (u_g(G + i + 2) - 2 * u_g(G + i + 1) + 2 * u_g(G + i - 1) -
               u_g(G + i - 2)) *
                dx3i -
              6 * u_g(i + G) * (u_g(i + G + 1) - u_g(i + G - 1)) * dxi;
   }
+  t_compute_rhs += MPI_Wtime();
 
   return rhs;
 }
 
+void write_output(const mpi_domain& domain, const xt::xtensor<double, 1>& x,
+                  const xt::xtensor<double, 1>& u, int t)
+{
+  std::ofstream out("u-" + std::to_string(t) + "-" +
+                    std::to_string(domain.rank()) + ".csv");
+  xt::dump_csv(out, xt::stack(xt::xtuple(x, u), 1));
+}
+
+void write_output_parallel(const mpi_domain& domain,
+                           const xt::xtensor<double, 1>& x,
+                           const xt::xtensor<double, 1>& u, int t)
+{
+  t_output -= MPI_Wtime();
+  auto X = xt::empty<double>({domain.N()});
+  auto U = xt::empty<double>({domain.N()});
+
+  MPI_Gather(x.data(), x.size(), MPI_DOUBLE, X.data(), x.size(), MPI_DOUBLE, 0,
+             domain.comm());
+  MPI_Gather(u.data(), u.size(), MPI_DOUBLE, U.data(), u.size(), MPI_DOUBLE, 0,
+             domain.comm());
+
+  if (domain.rank() == 0) {
+    std::ofstream out("u-" + std::to_string(t) + ".csv");
+    xt::dump_csv(out, xt::stack(xt::xtuple(X, U), 1));
+  }
+  t_output += MPI_Wtime();
+}
+
 int main(int argc, char** argv)
 {
-  const int N = 128;          // number of grid points
+  const int N = 12800;        // number of grid points
   const double L = 2. * M_PI; // total size of domain
-  const int n_timesteps = 100000;
-  const int output_every = 1000;
+  const int n_timesteps = 100;
+  const int output_every = -10;
 
   MPI_Init(&argc, &argv);
   mpi_domain domain(MPI_COMM_WORLD, N, L);
@@ -54,12 +91,13 @@ int main(int argc, char** argv)
   // a very stringent CLF condition
   double dt = .1 * std::pow(domain.dx(), 3);
 
+  double t_beg = MPI_Wtime();
   for (int n = 0; n < n_timesteps; n++) {
-    if (n % output_every == 0) {
-      std::ofstream out("u-" + std::to_string(n) + "-" +
-                        std::to_string(domain.rank()) + ".csv");
-      xt::dump_csv(out, xt::stack(xt::xtuple(x, u), 1));
-      std::cout << "writing output " << n << "\n";
+    if (output_every > 0 && n % output_every == 0) {
+      write_output_parallel(domain, x, u, n);
+      if (domain.rank() == 0) {
+        std::cout << "writing output " << n << "\n";
+      }
     }
 
     // advance one timestep
@@ -68,6 +106,13 @@ int main(int argc, char** argv)
     auto k3 = rhs(domain, u + .5 * dt * k2);
     auto k4 = rhs(domain, u + dt * k3);
     u += (1. / 6.) * dt * (k1 + 2. * k2 + 2. * k3 + k4);
+  }
+  double t_end = MPI_Wtime();
+  if (domain.rank() == 0) {
+    std::cout << "entire time integration: " << t_end - t_beg << " secs\n";
+    std::cout << "fill_ghosts            : " << t_fill_ghosts << " secs\n";
+    std::cout << "compute_rhs            : " << t_compute_rhs << " secs\n";
+    std::cout << "output                 : " << t_output << " secs\n";
   }
 
   MPI_Finalize();
